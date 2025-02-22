91b4acba26740692a0627fbf76a1c4b44834c81a
KrishPatel-AI <krish.work.ai@gmail.com>
2025-02-23 02:58:45 +0530

    Updated the Login/Register System

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -2,7 +2,7 @@ import { RegisterForm } from "@/components/RegisterForm"
 
 export default function page() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
+    <div className="flex  flex-col items-center justify-center gap-6 bg-background p-4">
       <div className="w-full max-w-sm">
         
         <RegisterForm />
